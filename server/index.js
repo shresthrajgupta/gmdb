bd6355187b8bc42bdfd7286ec020d5157912372b
shresthgupta9 <shresthgupta9@gmail.com>
2025-05-28 18:26:29 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 const express = require('express');
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(require("cors")());
+app.use(cors());
 
 app.use(cors({
   origin: 'https://gmdb-frontend.onrender.com',
