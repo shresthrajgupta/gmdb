85704e6023a21448dec74f79f7404b6c153445ad
shresthgupta9 <shresthgupta9@gmail.com>
2025-05-28 17:51:56 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(require("cors")());
